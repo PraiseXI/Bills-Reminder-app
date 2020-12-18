87dd1e7748002fd68adb589b96f06c1549544f42
praise <praisexi@outlook.com>
2020-12-18 20:46:12 +0000

    new code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bills_Reminder_app
+{
+    class bill
+    {
+        string title;
+        Double cost;
+        DateTime nextDue;
+        int daysuntil_next;
+        string how_often;
+        double amount_left;
+        string company_name;
+    }
+
+    class Program
+    {
+        static void startupQuestions()
+        {
+            Console.WriteLine("");
+        }
+        static void billInputQuestions()
+        {
+            Console.WriteLine("Enter the name of the bill");
+
+            Console.WriteLine("How much is each payment of the bill");
+        }
+        static void Main(string[] rgs)
+        {
+            billInputQuestions();
+        }
+    }
+}
