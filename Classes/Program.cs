d5fda572149ded173441b3679abb08d5ab90b0f8
praise <praisexi@outlook.com>
2020-12-21 21:02:29 +0000

    GITHUB CONNECTION TEST

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,14 +8,16 @@ namespace Bills_Reminder_app
         {
             Console.WriteLine("");
         }
+
         static void billInputQuestions()
         {
             Console.WriteLine("Enter the name of the bill");
-            Console.WriteLine("How much is each payment of the bill");
+            Console.WriteLine("H0ow much is each payment of the bill");
         }
         static void Main(string[] rgs)
         {
-            billInputQuestions();
+            //billInputQuestions();
+            Console.WriteLine("Hello wORLD");
         }
     }
 }
