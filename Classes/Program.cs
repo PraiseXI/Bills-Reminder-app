4cbfed7a7f3b17d2ed5d875c82d7385677ce04fe
praise <praisexi@outlook.com>
2021-01-20 16:30:15 +0000

    Serialized list of objects instead of 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,16 +10,19 @@ namespace Bills_Reminder_app
 {
     class Program
     {
-        static void startupQuestions()
-        {
-            Console.WriteLine("");
-        }
-
+        //TODO
         static void billInputQuestions()
         {
             Console.WriteLine("Enter the name of the bill");
             Console.WriteLine("H0ow much is each payment of the bill");
         }
+
+        //TODO
+        static void deserializer()
+        {
+
+        }
+
         static void Main(string[] rgs)
         {
             // to create a new object and insert values manually; will need to make this based on user input
@@ -60,9 +63,39 @@ namespace Bills_Reminder_app
             Netflix = (Bill)obj;
             reader.Close();
 
-            Console.WriteLine(Netflix.ToString());
+            List<Bill> MyBills = new List<Bill>
+            {
+                new Bill(1, "Phone Bill", 10.00, "05/02/2021", 16, 31, "N/A", "VOXI"),
+                new Bill(2, "Netflix Subscription", 2.00, "14/02/2021", 25, 31, "N/A", "Netflix"),
+                new Bill(3, "Spotify Subscription", 3.00, "27/01/2021", 7, 31, "N/A", "Spotify")
 
-            Console.ReadLine();
+            };
+
+             using (Stream fs = new FileStream(@"C:\Users\prais\Documents\Programming\Personal Projects\Bills Reminder app\data\MyBills.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Bill>));
+                serializer2.Serialize(fs, MyBills);
+            }
+
+            MyBills = null;
+
+            //Read Data from XML
+            XmlSerializer serializer3 = new XmlSerializer(typeof(List<Bill>));
+            using (FileStream fs2 = File.OpenRead(@"C:\Users\prais\Documents\Programming\Personal Projects\Bills Reminder app\data\MyBills.xml"))
+            {
+                MyBills = (List<Bill>)serializer3.Deserialize(fs2);
+            }
+
+            foreach(Bill x in MyBills)
+            {
+                Console.WriteLine(x.ToString());
+            }
+
+
+
+
+
+                Console.ReadLine();
 
             // billInputQuestions();
         }
